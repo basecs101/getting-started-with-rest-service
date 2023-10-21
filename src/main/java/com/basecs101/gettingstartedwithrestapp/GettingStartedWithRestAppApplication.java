5eab5e809e09380d5169e68358277c6f1c9746f5
Vikram Gupta <basecs101@gmail.com>
2023-10-21 20:16:15 +0530

    Getting started sample Spring boot application

---
+++ src/main/java/com/basecs101/gettingstartedwithrestapp/GettingStartedWithRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.basecs101.gettingstartedwithrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basecs101/gettingstartedwithrestapp/GettingStartedWithRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basecs101.gettingstartedwithrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
